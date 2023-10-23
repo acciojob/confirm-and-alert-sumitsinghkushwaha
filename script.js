1a4e3e4172ea849530fb6aba12ab26b001d5979c
Sumit Singh Kushwaha <68112081+sumitsinghkushwaha@users.noreply.github.com>
2023-10-23 20:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Display a confirmation dialog
+var userConfirmed = confirm("Do you want to proceed?");
+
+// Check the user's choice
+if (userConfirmed) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
